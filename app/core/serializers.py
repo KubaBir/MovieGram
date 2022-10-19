c0e3a037db928050bfec0da5e663177784bc8fea
Kuba Birecki <kubabir@outlook.com>
2022-10-19 22:22:36 +0200

    cleanup

--- app/core/serializers.py
+++ app/core/serializers.py
@@ -1,61 +1,76 @@
+
 from rest_framework import serializers
-from .models import Movie, Director, UserProfile, FriendRequest, Post, Comment
+
+from .models import Comment, Director, FriendRequest, Movie, Post, UserProfile
+
+
 class MovieSerializer(serializers.ModelSerializer):
+    director = serializers.CharField()
 
     class Meta:
-        model= Movie
-        fields='__all__'
+        model = Movie
+        fields = '__all__'
+
 
 class DirectorSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Director
-        fields=['name']
+        fields = ['name']
 
 
 class FriendRequestSerializerList(serializers.ModelSerializer):
+    sender = serializers.CharField(source='sender.name')
 
     class Meta:
         model = FriendRequest
-        fields = ['id','timestamp','sender']
+        fields = ['id', 'timestamp', 'sender']
 
 
 class FriendRequestSerializerSend(serializers.ModelSerializer):
     class Meta:
         model = FriendRequest
-        fields= ['sender','receiver']
+        fields = ['sender', 'receiver']
+        read_only_fields = ['sender']
+
 
 class AcceptingFriendRequestSerializer(serializers.ModelSerializer):
     class Meta:
         model = FriendRequest
-        fields= ['is_active']
+        fields = ['is_active']
+
 
 class CommentSerializer(serializers.ModelSerializer):
     class Meta:
         model = Comment
-        fields= ['user','comment_date','text']
-        extra_kwargs={
-            "user":{"read_only":True},
-            "comment_date":{"read_only":True}
+        fields = ['user', 'comment_date', 'text']
+        extra_kwargs = {
+            "user": {"read_only": True},
+            "comment_date": {"read_only": True}
         }
+
 
 class PostCreatingSerializer(serializers.ModelSerializer):
     class Meta:
         model = Post
-        fields=['user','post_date','title','movie','text']
-        extra_kwargs={
-            "user":{"read_only":True},
-            'post_date':{"read_only":True}
-        }
-class PostListingSerializer(serializers.ModelSerializer):
-    comments=CommentSerializer(many=True,required=False)
-    class Meta:
-        model = Post
-        fields= '__all__'
+        fields = ['user', 'post_date', 'title', 'movie', 'text']
         extra_kwargs = {
             "user": {"read_only": True},
             'post_date': {"read_only": True}
         }
+
+
+class PostListingSerializer(serializers.ModelSerializer):
+    comments = CommentSerializer(many=True, required=False)
+
+    class Meta:
+        model = Post
+        fields = '__all__'
+        extra_kwargs = {
+            "user": {"read_only": True},
+            'post_date': {"read_only": True}
+        }
+
     def update(self, validated_data):
         comment_data = validated_data.pop('comments')
         post = Post.objects.update(**validated_data)
@@ -66,12 +81,15 @@ class PostListingSerializer(serializers.ModelSerializer):
         return post
 
 
-
 class UserProfileSerializer(serializers.ModelSerializer):
-    posts = PostListingSerializer(many=True,required=False)
+    # Return user name instead of id
+    user = serializers.CharField()
+    posts = PostListingSerializer(many=True, required=False)
+
     class Meta:
         model = UserProfile
-        fields= ['user','top_movies','last_watched','friends','posts']
+        fields = ['user', 'top_movies', 'last_watched', 'friends', 'posts']
+        read_only_fields = ['user']
 
     def update(self, validated_data):
         posts_data = validated_data.pop('posts')
@@ -81,4 +99,3 @@ class UserProfileSerializer(serializers.ModelSerializer):
             Post.objects.create(userProfile=userprofile, **post)
 
         return userprofile
-
--- app/users/serializers.py
+++ app/users/serializers.py
@@ -1,34 +1,38 @@
 """Serializers for the user API View."""
-from rest_framework import serializers
-from django.contrib.auth import get_user_model, authenticate
-from django.utils.translation import gettext as _
 import sys
+
+from django.contrib.auth import authenticate, get_user_model
+from django.utils.translation import gettext as _
+from rest_framework import serializers
 from utils.scraping_movies import scraping_movies_func
 
-
 sys.path.append('..')
+
 
 class UserSerializer(serializers.ModelSerializer):
     """Serializer for the user object"""
 
     class Meta:
-        model=get_user_model()
-        fields=['email','password','name','filmweb_nick']
-        extra_kwargs={'password':{'write_only':True, 'min_length':5}}
+        model = get_user_model()
+        fields = ['email', 'password', 'name', 'filmweb_nick', 'id']
+        extra_kwargs = {
+            'password': {'write_only': True, 'min_length': 5},
+            'filmweb_nick': {'allow_null': True, 'required': False}
+        }
 
-    def create(self,validated_data):
+    def create(self, validated_data):
         """Create and return a user with encrypted password"""
         try:
             scraping_movies_func(self.validated_data['filmweb_nick'])
         except:
             pass
 
-
         return get_user_model().objects.create_user(**validated_data)
+
     def update(self, instance, validated_data):
         """Update and return user."""
-        password=validated_data.pop('password',None)
-        user = super().update(instance,validated_data)
+        password = validated_data.pop('password', None)
+        user = super().update(instance, validated_data)
 
         if password:
             user.set_password(password)
@@ -39,21 +43,21 @@ class UserSerializer(serializers.ModelSerializer):
 
 class AuthTokenSerializer(serializers.Serializer):
     """Serializer for the user authh token."""
-    email=serializers.EmailField()
-    password=serializers.CharField(style={"input_type":'password'},
-                                   trim_whitespace=False,)
-    def validate(self,attrs):
+    email = serializers.EmailField()
+    password = serializers.CharField(style={"input_type": 'password'},
+                                     trim_whitespace=False,)
+
+    def validate(self, attrs):
         """Validate and auth the user."""
-        email=attrs.get('email')
-        password=attrs.get('password')
-        user=authenticate(
+        email = attrs.get('email')
+        password = attrs.get('password')
+        user = authenticate(
             request=self.context.get('request'),
             username=email,
             password=password,
         )
         if not user:
-            msg=_('Unable to authenticate with provided credentials')
-            raise serializers.ValidationError(msg,code='authorization')
-        attrs['user']=user
+            msg = _('Unable to authenticate with provided credentials')
+            raise serializers.ValidationError(msg, code='authorization')
+        attrs['user'] = user
         return attrs
-
