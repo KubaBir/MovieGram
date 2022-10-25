dddefdaa94952384ff5ca093afc46b1a6efc1a0e
Jakub Birecki <kubabir@outlook.com>
2022-10-25 12:20:29 +0200

    celery

--- app/friend/urls.py
+++ app/friend/urls.py
@@ -2,9 +2,10 @@ from django.urls import path, include
 from . import views
 from rest_framework.routers import SimpleRouter
 router = SimpleRouter()
-router.register('friends_requests',views.FriendRequestViewSet,basename='friends_requests')
+router.register('friends_requests', views.FriendRequestViewSet,
+                basename='friends_requests')
 app_name = "friend"
 
 urlpatterns = [
-    path('',include(router.urls))
+    path('', include(router.urls))
 ]
