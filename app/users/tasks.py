b007f8681d24bfafe6216c18e3e4cf7620375183
Kuba Birecki <kubabir@outlook.com>
2022-10-28 14:44:22 +0200

    task fix

--- app/users/tasks.py
+++ app/users/tasks.py
@@ -1,5 +1,3 @@
-import time
-
 from app.celery import app as celery_app
 from django.contrib.auth import get_user_model
 from utils.scraping_movies import adding_to_profile_func
@@ -7,6 +5,5 @@ from utils.scraping_movies import adding_to_profile_func
 
 @celery_app.task
 def append_movies(filmweb_nick):
-    time.sleep(10)
     user = get_user_model().objects.get(filmweb_nick=filmweb_nick)
     adding_to_profile_func(filmweb_nick, user)
